bd259993b1da0b2b0fb437aac30d2d1a4048cbf5
JaeHyun Jeong <jjgus29@naver.com>
2025-06-01 14:10:33 +0900

    faet: 타임리프적용

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# KIS_API
+# kis-volume-rank
+한국투자증권 Open API 거래량 순위를 활용한 분석 💹
+
+
+![image](https://github.com/SeoYounSeok/kisvolumerank/assets/43161245/dc707782-0b1a-4e63-97a9-ba99e8b53196)
---
+++ src/main/java/com/seopport/kisvolumerank/controller/KisViewController.java
@@ -0,0 +1,29 @@
+package com.seopport.kisvolumerank.controller;
+
+import com.seopport.kisvolumerank.dto.ResponseOutputDTO;
+import com.seopport.kisvolumerank.service.KisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class KisViewController {
+
+    private final KisService kisService;
+
+    @Autowired
+    public KisViewController(KisService kisService) {
+        this.kisService = kisService;
+    }
+
+    @GetMapping("/volume-rank-view")
+    public String getVolumeRankView(Model model) {
+        // 비동기 데이터를 block()으로 동기 변환
+        List<ResponseOutputDTO> volumeRankList = kisService.getVolumeRank().block();
+        model.addAttribute("volumeRankList", volumeRankList);
+        return "volume-rank"; // templates/volume-rank.html
+    }
+}
